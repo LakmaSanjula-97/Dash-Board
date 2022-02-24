be1ce12d8303dbe81332d500c13fcab934740274
Lakma <lakmagamage97@gmail.com>
2022-02-25 01:52:01 +0530

    Committed

---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,73 @@
+import { lazy } from 'react';
+
+// project imports
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// dashboard routing
+const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
+
+// utilities routing
+const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
+const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
+const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
+const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
+const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
+
+// sample page routing
+const SamplePage = Loadable(lazy(() => import('views/sample-page')));
+
+const Register = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+
+const Sample = Loadable(lazy(() => import('views/utilities/Sample')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+        {
+            path: '/',
+            element: <DashboardDefault />
+        },
+        {
+            path: '/dashboard/default',
+            element: <DashboardDefault />
+        },
+        {
+            path: '/utils/util-typography',
+            element: <UtilsTypography />
+        },
+        {
+            path: '/utils/util-color',
+            element: <UtilsColor />
+        },
+        {
+            path: '/utils/util-shadow',
+            element: <UtilsShadow />
+        },
+        {
+            path: '/icons/tabler-icons',
+            element: <UtilsTablerIcons />
+        },
+        {
+            path: '/icons/material-icons',
+            element: <UtilsMaterialIcons />
+        },
+        {
+            path: '/sample-page',
+            element: <SamplePage />
+        },
+        {
+            path: '/pages/register/register3',
+            element: <Register />
+        },
+        {
+            path: '/utils/util-sample',
+            element: <Sample />
+        }
+    ]
+};
+
+export default MainRoutes;
